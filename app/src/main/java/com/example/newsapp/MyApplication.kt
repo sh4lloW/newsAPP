5de8fe4d3161263c485e515a974db0ac48bd1c6a
Blue <songofapple@outlook.com>
2022-12-04 18:53:11 +0800

    DeleteWarning

--- app/src/main/java/com/example/newsapp/MyApplication.kt
+++ app/src/main/java/com/example/newsapp/MyApplication.kt
@@ -1,10 +1,12 @@
 package com.example.newsapp
 
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
 class MyApplication: Application() {
     companion object {
+        @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
     }
 
--- app/src/main/java/com/example/newsapp/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapp/NewsAdapter.kt
@@ -2,7 +2,6 @@ package com.example.newsapp
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
