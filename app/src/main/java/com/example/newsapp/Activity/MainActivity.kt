913fc56614d8b105529eea7718291bf6e6ac87f9
lcx <13560528032@163.com>
2022-12-20 12:53:59 +0800

    取消滑动，我的关闭分类

--- app/src/main/java/com/example/newsapp/Activity/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/Activity/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.example.newsapp.Activity
 
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.newsapp.*
-import com.example.newsapp.model.News
-import com.example.newsapp.model.NewsResponse
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayout.Tab
-import com.google.gson.Gson
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import kotlin.concurrent.thread
-import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,8 +19,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contentViewpage = findViewById<ViewPager>(R.id.content_view_pager)
+        val contentViewpage = findViewById<NoSwipeViewPager>(R.id.content_view_pager)
         val tablayout = findViewById<TabLayout>(R.id.tab_layout)
+
+        //设置不可滑动
+        contentViewpage.setCanSwipe(false)
         //设置fragment页面的缓存数量
         contentViewpage.offscreenPageLimit = fragmentList.size
         contentViewpage.adapter = MyAdapter(supportFragmentManager)
@@ -63,6 +54,12 @@ class MainActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.menu_message -> contentViewpage.setCurrentItem(0,false)
                 R.id.menu_contacts -> contentViewpage.setCurrentItem(6,false)
+            }
+            //是否取消分类栏
+            when(it.itemId){
+                R.id.menu_message -> tablayout.isVisible = true
+                R.id.menu_contacts -> tablayout.isVisible = false
+
             }
         }
 
